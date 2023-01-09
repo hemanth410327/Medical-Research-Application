416d9ff88a7ca69489b695fda98a50b1689437c2
Hemanth T R <56040295+hemanth410327@users.noreply.github.com>
2023-01-09 16:06:36 +0530

    Add files via upload

---
+++ Hackathon/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    class main
+    {
+        public static void Main(string[] args)
+        {
+            UI.Run(args[0]);
+        }
+    }
+}
---
+++ Hackathon/MyHelperClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHelperClassRepo
+{
+    class MyHelperClass
+    {
+        internal static int GetNumber(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        internal static string GetValue(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+    }
+}
