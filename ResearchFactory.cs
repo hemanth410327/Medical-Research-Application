a1c8655f6d8f779145dd3baaedaf77e0df82ada7
Hemanth T R <56040295+hemanth410327@users.noreply.github.com>
2023-01-09 16:03:54 +0530

    Add files via upload

---
+++ ResearchFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    class ResearchFactory
+    {
+        public static object GetInstance(int option)
+        {
+            switch (option)
+            {
+                case 1: return new DiseaseManager();
+
+                case 2: return new SymptomsManager();
+
+                default:
+                    Console.WriteLine("Object you are requesting is not available");
+                    break;
+            }
+            return null;
+        }
+    }
+}
